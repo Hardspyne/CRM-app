2bda18b8bbd8ae604da5998932ac7f171cb878af
Hardspyne <79159707922@yandex.ru>
2019-04-10 19:25:24 +0300

    add saveCustomer method in DAO and Service

--- src/main/java/com/dao/CustomerDAOImpl.java
+++ src/main/java/com/dao/CustomerDAOImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,5 +21,11 @@ public class CustomerDAOImpl implements CustomerDAO {
 
         Query<Customer> query = session.createQuery("from Customer", Customer.class);
         return query.getResultList();
+    }
+
+    @Override
+    public void saveCostumer(Customer customer) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(customer);
     }
 }
