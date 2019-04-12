fa691b70f65bda8e6c1c127071216d6044e2ca16
Hardspyne <79159707922@yandex.ru>
2019-04-12 21:03:50 +0300

    add security support

---
+++ src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource myDataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(myDataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("GUEST", "MANAGER", "ADMIN")
+                .antMatchers("/customer/list").hasAnyRole("ADMIN", "GUEST", "MANAGER")
+                .antMatchers("/customer/showFormForAdd").hasAnyRole("ADMIN,MANAGER")
+                .antMatchers("/customer/showFormForUpdate").hasAnyRole("ADMIN,MANAGER")
+                .antMatchers("/customer/delete").hasAnyRole("ADMIN")
+                .and()
+                .formLogin().loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
+    }
+}
